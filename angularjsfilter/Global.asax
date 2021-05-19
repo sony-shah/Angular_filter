0f8331baa481f7b71a166a388d1120000ddfbe1b
UJustBe <UJustBe@DESKTOP-FRVLAGQ>
2021-05-19 13:30:49 +0530

    Add project files.

---
+++ angularjsfilter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="angularjsfilter.Global" Language="C#" %>
---
+++ angularjsfilter/Scripts/JavaScript.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="angular.min.js" />
+var filter = angular.module("mymodule", []);
+
+filter.controller("mycontroller", function ($scope) {
+
+    var emp = [
+
+        { firstName: "soni", lastName: "Shah", gender: "female", salary: "500000" },
+        { firstName: "varun", lastName: "yadav", gender: "male", salary: "50000" },
+        { firstName: "shivam", lastName: "Dixit", gender: "male", salary: "450000" },
+        { firstName: "kusum", lastName: "sharma", gender: "female", salary: "5000" },
+        { firstName: "pinki", lastName: "sahani", gender: "female", salary: "600000" },
+        { firstName: "pooja", lastName: "gupta", gender: "female", salary: "100000" }
+
+    ];
+    $scope.emp = emp;
+});
+   
